ed3f1dffd789573f5fc53bd4c1876bab3c981e99
sacerba <a.scerba02@gmail.com>
2019-11-23 19:42:09 -0600

    Added documentation

--- servercode/SQM LU Data Logger/read.py
+++ servercode/SQM LU Data Logger/read.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
 
+# Import required libraries/modules
 import serial
 import time
 import os
 
+# Main read function. Sends a string to the defined port and
+# prints the resulting output to a file "log.txt" and the terminal.
 def serialread():
     ser = serial.Serial(
-        port=interface(NIX),\ # change between NIX and DOS
+        port='/dev/tty',\
         baudrate=115200,\
         parity=serial.PARITY_NONE,\
         stopbits=serial.STOPBITS_ONE,\
         bytesize=serial.EIGHTBITS,\
             timeout=1)
 
-    print("connected to: " + ser.portstr)
+    log = open("log.txt", "a")
+
+    print("connected to: " + ser.portstr, file=log)
 
     ser.write(str.encode("rx\n"))
     ser.flush()
 
-    print(ser.readline())
+    print(ser.readline(), file=log)
 
     ser.close()
 
+    log.close()
+
+# Runs serialread() function every second.
 while True:
     serialread()
     time.sleep(1)
