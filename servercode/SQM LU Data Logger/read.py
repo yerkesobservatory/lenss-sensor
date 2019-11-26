7117394d29a2d0507fa2f9fcf14b04af197f9112
sacerba <a.scerba02@gmail.com>
2019-11-26 17:09:49 -0600

    added autodetection of sensor

--- servercode/SQM LU Data Logger/read.py
+++ servercode/SQM LU Data Logger/read.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
 # Import required libraries/modules
+from datetime import datetime
+from datetime import date
+from datetime import time
 import platform
 import serial
+import serial.tools.list_ports
 import time
 import os
+
+now = datetime.now()
+log = open('./log_' + now.strftime('%Y-%m-%d') + '.txt', 'a')
 
 # Automatically selects port syntax based on OS
 # IMPORTANT!!! - Missing ability to change ports depending on machine
 # and sensor!
 def port():
     if str(platform.system()) == 'Windows':
-        return 'COM4'
+        for p in ports:
+            if 'USB Serial' in p.description:
+                q = str(p)
+                return(q[:4])
     else:
-        return '/dev/tty.usbserial-A5055IX5A'
+        return  '/dev/ttyS4' #'/dev/tty.usbserial-A5055IX5A' # /dev/ttyS(Port Number) for WSL
 
 # Main read function. Sends a string to the defined port and
 # prints the resulting output to a file "log.txt" and the terminal.
@@ -25,15 +35,14 @@ def serialread():
         stopbits=serial.STOPBITS_ONE,\
         bytesize=serial.EIGHTBITS,\
             timeout=1)
-    log = open('./log.txt', 'a')
     print("connected to: " + ser.portstr)
     log.write("connected to: " + str(ser.portstr) + "\n")
     ser.write(str.encode("rx\n"))
     ser.flush()
     print(ser.readline())
     log.write(str(ser.readline()) + "\n")
+    #log.close()
     ser.close()
-    log.close()
 
 # Runs serialread() function every second.
 while True:
