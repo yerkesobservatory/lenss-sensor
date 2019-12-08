ea06ea8540803a367686d4f76e761c572e63c000
sacerba <a.scerba02@gmail.com>
2019-12-07 23:58:18 -0600

    Began serial comm integration

--- sensorcode/TSL237__Arduino_Uno_Compatible_.ino
+++ sensorcode/TSL237__Arduino_Uno_Compatible_.ino
@@ -1,3 +1,11 @@
+// TODO:
+//
+// Add more comments for what functions do
+// -or-
+// Create README for the program with info on what 
+// each function does and some clarification on 
+// pin assignments
+
 volatile unsigned long cnt = 0;
 unsigned long oldcnt = 0;
 unsigned long t = 0;
@@ -6,18 +14,24 @@ int DigPin = 2;
 int IntPin = 0;
 const int TempSensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
 
-void irq1()
-{
+int inByte = 0; //incoming serial byte
+
+void irq1() {
   cnt++;
 }
 
 ///////////////////////////////////////////////////////////////////
 //
-// SETUP
+// SETUP: Start and wait for serial connection, assign pins, and 
+// setup attatch interupt.
 //
-void setup() 
-{
-  Serial.begin(115200);
+///////////////////////////////////////////////////////////////////
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+  establishcontact();
   Serial.println("START");
   delay(1000);
   pinMode(DigPin, INPUT);
@@ -27,36 +41,51 @@ void setup()
 
 ///////////////////////////////////////////////////////////////////
 //
-// MAIN LOOP
+// MAIN LOOP: Something goes here
 //
-void loop() 
-{
-  if (millis() - last >= 1000)
-  {
-    last = millis();
-    t = cnt;
-    unsigned long hz = t - oldcnt;
-    Serial.print("FREQ: "); 
-    Serial.print(hz);
-    Serial.print(" = "); 
-    Serial.print((hz+50)/100);  // +50 == rounding last digit
-    Serial.print(" mW/m2; ");
-    oldcnt = t;
+///////////////////////////////////////////////////////////////////
+void loop() {
+  if (Serial.available() <= 0) {
+    if (millis() - last >= 1000) {
+      last = millis();
+      t = cnt;
+      unsigned long hz = t - oldcnt;
+      Serial.print("FREQ: "); 
+      Serial.print(hz);
+      Serial.print(" = "); 
+      Serial.print((hz+50)/100);  // +50 == rounding last digit
+      Serial.print(" mW/m2; ");
+      oldcnt = t;
 
-    int reading = analogRead(TempSensorPin);
-    float voltage = reading * 5.0;
-    voltage /= 1024.0;
-    Serial.print("\t");
-    Serial.print(voltage); Serial.print(" volts; ");
-    float tempC = (voltage - 0.5) * 100;
-    Serial.print(tempC); Serial.print(" degrees C; ");
-    float tempF = 9/5 * tempC + 32;
-    Serial.print(tempF); Serial.println(" degrees F");
+      int reading = analogRead(TempSensorPin);
+      float voltage = reading * 5.0;
+      voltage /= 1024.0;
+      Serial.print("\t");
+      Serial.print(voltage); Serial.print(" volts; ");
+      float tempC = (voltage - 0.5) * 100;
+      Serial.print(tempC); Serial.print(" degrees C; ");
+      float tempF = 9/5 * tempC + 32;
+      Serial.print(tempF); Serial.println(" degrees F");
     
-    if (millis() % 3600000 >= 0)
-    {
-      cnt = 0;
-      oldcnt = 0;
+      if (millis() % 3600000 >= 0) {
+        cnt = 0;
+        oldcnt = 0;
+      }
     }
+  } else {
+    // not sure yet
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+//
+// Establish Contact: send 'calibration' character for recieving 
+// program
+//
+///////////////////////////////////////////////////////////////////
+void establishcontact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');
+    delay(300);
   }
 }
