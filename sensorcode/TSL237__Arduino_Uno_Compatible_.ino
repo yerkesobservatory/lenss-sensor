b847ca5d4ce6020fc757b099f2dd8b24418e14b8
blimpmaster <joe.murphy1415@gmail.com>
2019-12-28 15:37:52 -0600

    Update TSL237__Arduino_Uno_Compatible_.ino
    Now will shut off if too bright

--- sensorcode/TSL237__Arduino_Uno_Compatible_.ino
+++ sensorcode/TSL237__Arduino_Uno_Compatible_.ino
@@ -3,7 +3,10 @@ unsigned long oldcnt = 0;
 unsigned long t = 0;
 unsigned long last;
 int DigPin = 2;
+int TslPwr = 8;
 int IntPin = 0;
+const int TempSensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
+unsigned long hz;
 
 void irq1()
 {
@@ -20,7 +23,9 @@ void setup()
   Serial.println("START");
   delay(1000);
   pinMode(DigPin, INPUT);
+  pinMode(TslPwr, OUTPUT);
   digitalWrite(DigPin, HIGH);
+  pinMode(TslPwr, HIGH);
   attachInterrupt(IntPin, irq1, RISING);
 }
 
@@ -30,18 +35,46 @@ void setup()
 //
 void loop() 
 {
+  float lux = analogRead(A5);
+  float AVolt = lux * 5/1023;
+  
+  if (AVolt > 2.5)
+  {
+    digitalWrite(TslPwr, LOW);
+    hz = 0;
+  }
+  else
+  {
+    digitalWrite(TslPwr, HIGH);
+    if (millis() - last >= 1000)
+    {
+      t = cnt;
+      hz = t - oldcnt;
+    }
+  }
+  
   if (millis() - last >= 1000)
   {
+    Serial.print(AVolt); Serial.print(" | ");
     last = millis();
-    t = cnt;
-    unsigned long hz = t - oldcnt;
     Serial.print("FREQ: "); 
     Serial.print(hz);
-    Serial.print("\t = "); 
+    Serial.print(" = "); 
     Serial.print((hz+50)/100);  // +50 == rounding last digit
-    Serial.println(" mW/m2");
+    Serial.print(" mW/m2; ");
     oldcnt = t;
-    if (millis() % 3600000 >= 0)
+
+    int reading = analogRead(TempSensorPin);
+    float voltage = reading * 5.0;
+    voltage /= 1024.0;
+    Serial.print("\t");
+    Serial.print(voltage); Serial.print(" volts; ");
+    float tempC = (voltage - 0.5) * 100;
+    Serial.print(tempC); Serial.print(" degrees C; ");
+    float tempF = 9/5 * tempC + 32;
+    Serial.print(tempF); Serial.println(" degrees F");
+    
+    if (millis() % 3600000 == 0)
     {
       cnt = 0;
       oldcnt = 0;
