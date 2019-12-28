74e7049af8e7063030fa082ed45038b89c3e34c3
blimpmaster <joe.murphy1415@gmail.com>
2019-12-28 16:17:02 -0600

    Add files via upload
    pushes data to a file

--- servercode/ARD Data Logger/ARDread.py
+++ servercode/ARD Data Logger/ARDread.py
@@ -2,8 +2,8 @@ import serial
 import RPi.GPIO as GPIO
 import time
 
-# ser=serial.Serial("/dev/ttyACM0", 115200)
-ser=serial.Serial("/dev/ttyACM1", 115200)
+ser=serial.Serial("/dev/ttyACM0", 115200)
+#ser=serial.Serial("/dev/ttyACM1", 115200)
 # ttyAMA0
 ser.baudrate=115200
 
@@ -12,11 +12,10 @@ GPIO.setmode(GPIO.BOARD)
 while True:
 
     read_ser=ser.readline()
-    print(read_ser.decode("utf-8"))
-    #print(read_ser)
+    read_fmtd = read_ser.decode("utf-8")
+    print(read_fmtd)
+    with open("TestDoc1.txt", "a") as text_file:
+        print(read_fmtd, file=text_file)
 
-#Also read other SQM
 #Send to a file
 #Use command line argument optionally for port & filename
-#(Arduino) shut off sensor when light too intense using digital pin for power
-#(Arduino) use less precise photocell to determine when to power tsl
