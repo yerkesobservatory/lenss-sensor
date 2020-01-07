d5eb74733277867b930632d6255e347e7ad063ca
blimpmaster <joe.murphy1415@gmail.com>
2020-01-07 17:14:24 -0600

    Commented for easier use by others

--- sensorcode/TSL237_Uno_CSVfmtd.ino
+++ sensorcode/TSL237_Uno_CSVfmtd.ino
@@ -22,11 +22,11 @@ void setup()
   Serial.begin(115200);
   Serial.println("Light Sensor Voltage (volts),,Frequency (hz),Luminosity (mW/m2),,Temp Sensor Voltage (volts),Temperature (Degrees C),Temperature (Degrees F)");
   delay(1000);
-  pinMode(DigPin, INPUT);
-  pinMode(TslPwr, OUTPUT);
-  digitalWrite(DigPin, HIGH);
-  pinMode(TslPwr, HIGH);
-  attachInterrupt(IntPin, irq1, RISING);
+  pinMode(DigPin, INPUT); //Sets the corresponding pin to receive data from the light-to-frequency (tsl) sensor
+  pinMode(TslPwr, OUTPUT); //Sets the power pin of the tsl to draw power from a digital pin on the arduino
+  digitalWrite(DigPin, HIGH); //Turns on DigPin
+  pinMode(TslPwr, HIGH); //Turns on TslPwr
+  attachInterrupt(IntPin, irq1, RISING); //Every time the interrupt pin of the tsl registers, the arduino will drop everything and bump up the frequency before proceeding.
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -36,17 +36,17 @@ void setup()
 void loop() 
 {
   float lux = analogRead(A5);
-  float AVolt = lux * 5/1023;
+  float AVolt = lux * 5/1023; //Converts from within the 0-1023 bounds of analog data to the 0-5 bounds of voltage
   
-  if (AVolt > 2.5)
+  if (AVolt > 2.5) //Sets an upper bound for what the light sensor is allowed to have to prevent overload
   {
-    digitalWrite(TslPwr, LOW);
+    digitalWrite(TslPwr, LOW); //Kills tsl sensor if bound exceeded
     hz = 0;
   }
   else
   {
     digitalWrite(TslPwr, HIGH);
-    if (millis() - last >= 1000)
+    if (millis() - last >= 1000) //Used to measure 1 second around interrupts
     {
       t = cnt;
       hz = t - oldcnt;
@@ -55,22 +55,23 @@ void loop()
   
   if (millis() - last >= 1000)
   {
-    Serial.print(AVolt); Serial.print(",,");
+    Serial.print(AVolt); Serial.print(",,"); //Commas used for separating columns in a csv file
     last = millis();
     Serial.print(hz); Serial.print(",");
-    Serial.print((hz+50)/100); Serial.print(",,"); 
+    Serial.print((hz+50)/100); Serial.print(",,"); //The original creator of this chunk of the code used 1/100 as a reasonable conversion factor for hz-->mW/m2. 
+                                                   //Integer division just moves place values and cuts the decimal, so +50 allows the number to round and remain an integer.
     oldcnt = t;
 
     int reading = analogRead(TempSensorPin);
     float voltage = reading * 5.0;
-    voltage /= 1024.0;
+    voltage /= 1024.0; //Converts to volts similarly to how the analog voltage readings from the light sensor did earlier did earlier
     Serial.print(voltage); Serial.print(",");
     float tempC = (voltage - 0.5) * 100;
     Serial.print(tempC); Serial.print(",");
     float tempF = 1.8 * tempC + 32;
     Serial.println(tempF);
     
-    if (millis() % 3600000 == 0)
+    if (millis() % 3600000 == 0) //Every hour...
     {
       cnt = 0;
       oldcnt = 0;
