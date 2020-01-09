945aa01f527219e4ee3af3819e6a0ee3c37f034f
blimpmaster <joe.murphy1415@gmail.com>
2020-01-09 17:24:13 -0600

    Now makes csv files and uses config

--- servercode/ARD Data Logger/ArdReadTime.py
+++ servercode/ARD Data Logger/ArdReadTime.py
@@ -2,6 +2,7 @@ import serial
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
+from config import port
 
 name=input("Please enter the name of the file to store current data or enter /time to use the current date and time:    ")
 
@@ -11,11 +12,9 @@ if (name=="/time"):
 else:
     filename=name
     
-suffilename=filename+".txt"
+suffilename=filename+".csv"
 
-portin=input("Please complete current port name to allow reading:    /dev/tty")
-port="/dev/tty"+portin
-ser=serial.Serial(port, 115200)
+ser=serial.Serial(port, 115200) #create a config file with the variable "port" to store personal port data
 #ser=serial.Serial("/dev/ttyACM1", 115200)
 ser.baudrate=115200
 
@@ -26,7 +25,7 @@ while True:
     read_ser=ser.readline()
     read_fmtd = read_ser.decode("utf-8")
     print(read_fmtd)
-    with open(suffilename, "a") as text_file:  #a for append
-        print(read_fmtd, file=text_file)
+    with open(suffilename, "a") as csv_file:  #a for append
+        print(read_fmtd, file=csv_file)
 
 #Use command line argument optionally for port & filename
