e7219bc32395ad24213b52828fad356101947cf9
blimpmaster <joe.murphy1415@gmail.com>
2020-01-30 17:06:28 -0600

    Add files via upload

--- servercode/ARD Data Logger/ARDread.py
+++ servercode/ARD Data Logger/ARDread.py
@@ -1,21 +1,49 @@
 import serial
 import RPi.GPIO as GPIO
 import time
-
-ser=serial.Serial("/dev/ttyACM0", 115200)
-#ser=serial.Serial("/dev/ttyACM1", 115200)
-# ttyAMA0
-ser.baudrate=115200
+from datetime import datetime
+import serial.tools.list_ports
+import configparser
+import  os
+import sys
 
 GPIO.setmode(GPIO.BOARD)
 
-while True:
+if len(sys.argv) < 2:
+    print('WARNING: Must give filepathname to valid config file')
+    print('Usage:\n  python SQM_LU_LOGGER.py ../config/serverconfig.ini')
+    print('  replace with your own config file as appropriate')
+    exit(1)
 
+Config_FilePathName = sys.argv[1]
+config = configparser.ConfigParser()
+config.read(Config_FilePathName)
+
+
+now = datetime.now()
+print(config['arddatalogger']['arduinoport'])
+def port(config):
+    try:
+        return config['arddatalogger']['arduinoport']
+    except:
+        exit(1)
+        
+def serialread(config):
+    ser  =  serial . Serial (
+        port=port(config),\
+        baudrate=9600,\
+        parity=serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+            timeout=1)
     read_ser=ser.readline()
     read_fmtd = read_ser.decode("utf-8")
     print(read_fmtd)
-    with open("TestDoc1.txt", "a") as text_file:
-        print(read_fmtd, file=text_file)
+    log = open('TestlogA2.txt', 'a')
+    log.write(read_fmtd)
+    log.close()
 
-#Send to a file
-#Use command line argument optionally for port & filename
+while True:
+
+    serialread(config)
+    time.sleep(1)
