0ff0e2976af8c98bae7d23a44a0e892c644af5b0
blimpmaster <joe.murphy1415@gmail.com>
2020-02-18 16:52:48 -0600

    Fixed stranch git issue

--- sensorcode/TSL237__Arduino_Uno_Compatible_.ino
+++ sensorcode/TSL237__Arduino_Uno_Compatible_.ino
@@ -13,10 +13,7 @@ int DigPin = 2;
 int TslPwr = 8;
 int IntPin = 0;
 const int TempSensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
-<<<<<<< HEAD
 unsigned long hz;
-=======
->>>>>>> ard-serial-communication
 
 int inByte = 0; //incoming serial byte
 
@@ -24,12 +21,6 @@ void irq1() {
   cnt++;
 }
 
-///////////////////////////////////////////////////////////////////
-//
-// SETUP: Start and wait for serial connection, assign pins, and 
-// setup attatch interupt.
-//
-///////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
@@ -44,57 +35,6 @@ void setup() {
   pinMode(TslPwr, HIGH);
   attachInterrupt(IntPin, irq1, RISING);
 }
-
-///////////////////////////////////////////////////////////////////
-//
-// MAIN LOOP: Something goes here
-//
-<<<<<<< HEAD
-void loop() 
-{
-  float lux = analogRead(A5);
-  float AVolt = lux * 5/1023;
-  
-  if (AVolt > 2.5)
-  {
-    digitalWrite(TslPwr, LOW);
-    hz = 0;
-  }
-  else
-  {
-    digitalWrite(TslPwr, HIGH);
-    if (millis() - last >= 1000)
-    {
-      t = cnt;
-      hz = t - oldcnt;
-    }
-  }
-  
-  if (millis() - last >= 1000)
-  {
-    Serial.print(AVolt); Serial.print(" | ");
-    last = millis();
-    Serial.print("FREQ: "); 
-    Serial.print(hz);
-    Serial.print(" = "); 
-    Serial.print((hz+50)/100);  // +50 == rounding last digit
-    Serial.print(" mW/m2; ");
-    oldcnt = t;
-
-    int reading = analogRead(TempSensorPin);
-    float voltage = reading * 5.0;
-    voltage /= 1024.0;
-    Serial.print("\t");
-    Serial.print(voltage); Serial.print(" volts; ");
-    float tempC = (voltage - 0.5) * 100;
-    Serial.print(tempC); Serial.print(" degrees C; ");
-    float tempF = 1.8 * tempC + 32;
-    Serial.print(tempF); Serial.println(" degrees F");
-    
-    if (millis() % 3600000 == 0)
-    {
-=======
-///////////////////////////////////////////////////////////////////
 void loop() {
   if (Serial.available() <= 0) {
     inByte = Serial.read();
@@ -123,7 +63,6 @@ void loop() {
       Serial.write(tempC);
       Serial.write(tempF);
     
->>>>>>> ard-serial-communication
       cnt = 0;
 
   } else {
@@ -131,12 +70,6 @@ void loop() {
   }
 }
 
-///////////////////////////////////////////////////////////////////
-//
-// Establish Contact: send 'calibration' character for recieving 
-// program
-//
-///////////////////////////////////////////////////////////////////
 void establishcontact() {
   while (Serial.available() <= 0) {
     Serial.print('A');
