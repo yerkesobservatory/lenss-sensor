ce76831895d8ab538eef5d18106cfd98e5674ba0
berthoud <mgb11@cornell.edu>
2021-03-18 16:58:59 -0500

    Updated watchdog with comments.

--- servercode/watchdog/watchdog.py
+++ servercode/watchdog/watchdog.py
@@ -1,3 +1,17 @@
+""" WATCHDOG
+    ========
+    
+    This is the watchdog program for the LENS SENSOR project.
+    
+    The program does the following:
+    JOE WRITE HERE WHAT THE PROGRAM DOES AND WHICH TECHNOLOGIES IT USES
+    
+    Authors: Joe Murphy
+"""
+
+
+#### Setup
+
 # If pulled from github, delete the token file. credentials.json is connected to lenss@glaseducation.org
 
 from __future__ import print_function #imports for Google Drive API functionality
@@ -14,40 +28,56 @@ import os, sys
 import configparser
 from getpass import getpass
 
-def getSearchQuery(): #finds what to search for in Google Drive API
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'] #limits what email can do on google drive
+tokenfile = 'token.json'
+credentialsfile = 'credentials.json'
+
+#### Function Definitions
+
+def getSearchQuery(): 
+    """ This functions generates the filename to search for in the Google Drive
+        The filename is based on the date.
+    """
     yesterday = datetime.now() - timedelta(1) #uses today's date and the equivalent to one day to find yesterday's date
     time = yesterday.strftime('%Y-%m-%d_LENSSTSL') #assembles time into a string for search
     return time #end result
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'] #limits what email can do on google drive
-
-def getFileNames(): #API search for yesterday's files
+def getFileNames():
+    """ Looks for files uploaded in the last day on google drive.
+        The function returns a list of filenames.
+    """
+    #### Get Google Drive API Credentials
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(tokenfile):
+        creds = Credentials.from_authorized_user_file(tokenfile, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                credentialsfile, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(tokenfile, 'w') as token:
             token.write(creds.to_json())
 
+    #### Get information from the API
     service = build('drive', 'v3', credentials=creds)
 
     # Call the Drive v3 API
-    results = service.files().list(
-        q="name contains '{0}'".format(getSearchQuery()), spaces='drive', fields="nextPageToken, files(id, name)").execute() # searches API: search query input into q
+    # searches API: search query input into q
+    results = service.files().list(q="name contains '{0}'".format(getSearchQuery()), 
+                                   spaces='drive', 
+                                   fields="nextPageToken, files(id, name)").execute()
+    # Get the list of files
     items = results.get('files', [])
 
+    # Make response string
     if not items:
         outcome = "No files found."
     else:
@@ -57,7 +87,10 @@ def getFileNames(): #API search for yesterday's files
 
     return outcome
 
-def sendStatus(): # sends email
+def sendStatus():
+    """ This function sends the mail with the daily status report
+    """
+    # Define mail message
     message = """\
     Subject: Daily Status Report
 
@@ -68,16 +101,19 @@ def sendStatus(): # sends email
     #readEmail = configparser.ConfigParser()
     #readEmail.read(Email_FilePathName)
 
+    # Define email credentials
     password = getpass() #obscured input for password
     sender = 'lenss@glaseducation.org'
     receivers = ['joe@glaseducation.org', 'adam@glaseducation.org']
 
+    # Use the SMTP library to send the email
     smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
     smtp_server.ehlo()
     smtp_server.starttls()
     smtp_server.ehlo
     smtp_server.login(sender, password)
     for i in range(len(receivers)):
+        # Send email to each receiver
         print(receivers[i])
         smtp_server.sendmail(sender, receivers[i], message)
     smtp_server.close()
