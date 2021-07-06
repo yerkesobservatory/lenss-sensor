a1f6a47654d98d8418b215668f60aad2892082a7
LENSS <lenss@glaseducation.org>
2021-07-06 17:54:26 -0500

    Initialization folder removed due to changes in previous push

--- raspi/Reconfiguration/setport.sh
+++ raspi/Reconfiguration/setport.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
 
 port=$(echo $(ls /dev | grep ACM))
-sed "6s/.*/arduinoport = \/dev\/${port}/" /home/pi/lenss-sensor/config/serverconfig.ini
+sed -i "6s/.*/arduinoport = \/dev\/${port}/" /home/pi/lenss-sensor/config/serverconfig.ini
