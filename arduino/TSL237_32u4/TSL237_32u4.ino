fe9eb3c487f52700d8452a39a35455aa2c935096
blimpmaster <joe.murphy1415@gmail.com>
2021-08-12 14:44:46 -0500

    Add files via upload

--- arduino/TSL237_32u4/TSL237_32u4.ino
+++ arduino/TSL237_32u4/TSL237_32u4.ino
@@ -6,7 +6,7 @@
 
 volatile unsigned long cnt = 0; //preps variables; cnt is photon count,
 unsigned long t = 0; //t is used to copy values from other variables,
-unsigned long last = 0; //last is the recorded time of the last datapoint
+unsigned long last = 0; //last is the benchmark for how much time passes
 
 int DigPin = 3; //DigPin and IntPin refer to the same spot, but interrupt and digital pins have different names
 int TslPwr = 5; //digital pin used to power tsl for convenient on/off switch
@@ -14,7 +14,7 @@ int IntPin = 0;
 
 unsigned long hz; //frequency of photon counts per second
 unsigned long oldcnt; //last recorded photon count
-unsigned long T, Tlast; //time storage
+unsigned long T, Tlast; //time storage: T is current time and Tlast is the time from last loop
 int delta; //time elapsed since last data point
 String message; //message carried to pi
 float lux; //raw output from photoresistor circuit
@@ -50,7 +50,7 @@ void loop() //loop is the other special function of arduinos; it repeats indefin
   volt = lux * 3.3/1023; //corrects lux from a scale of 1023 (analog scale) to 3.3 (voltage received)
   sensors.requestTemperatures(); //reads temp sensor
   
-  if (volt > 2.5)
+  if (volt > 2.0)
   {
     digitalWrite(TslPwr, LOW); //shuts down tsl when it's too bright
   }
@@ -63,15 +63,12 @@ void loop() //loop is the other special function of arduinos; it repeats indefin
   delta = T - last;  //gets elapsed time stored in delta
   if (delta >= 1000) //after 1 second
   {
-    //last = T; 
-    while( T-last > 1000) { last += 1000; }
-    //last += 1000;
+    while( T-last > 1000) { last += 1000; } //increases last by 1 second intervals; while loop prioritizes action
     t = cnt; //because interrupt is incredibly fast, t stores the count right when the reading is taken
     hz = round((t - oldcnt)*1000/(T-Tlast)); //takes photon counts and puts them in Hz (#/s)
-    //hz = t - oldcnt; //takes photon counts and puts them in Hz (#/s)
-    oldcnt = t;
-    message = String(volt)+","+String(hz)+","+String(sensors.getTempCByIndex(0))+","+String(T-Tlast)+","+
-              String(TSLserialNR);//+","+String(T/1000.0)+","+String(T-Tlast);
+    oldcnt = t; //remembers last count to calculate Hz next loop
+    message = String(volt)+","+String(hz)+","+String(sensors.getTempCByIndex(0))+","+
+              String(TSLserialNR);
     Serial.println(message); //assembles message and sends it to the pi
     Tlast = T;
   }
