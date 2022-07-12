f2ef73f881644cfd33fa7f0cd55da601312f3cc5
ascerba <a.scerba02@gmail.com>
2022-07-12 09:56:00 -0500

    Added a falure exit status if the Arduino is not connected.

--- raspi/Reconfiguration/setport.sh
+++ raspi/Reconfiguration/setport.sh
@@ -1,4 +1,13 @@
-#!/bin/bash/
+#!/bin/bash
 
-port=$(echo $(ls /dev | grep ACM))
-sed -i "6s/.*/arduinoport = \/dev\/${port}/" /home/pi/lenss-sensor/config/serverconfig.ini
+port=$e(echo $(ls /dev | grep ACM))
+
+if [[ -z "$port" ]]; then
+  echo "No Arduino connected."
+  exit 1
+else
+  sed -i "6s/.*/arduinoport = \/dev\/${port}/" /home/pi/Programs/serverconfig.ini
+fi
+
+echo "Port set successfully."
+exit 0
