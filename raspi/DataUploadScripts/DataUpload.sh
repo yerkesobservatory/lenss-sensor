9c5b25054f2bd470860f3bad5e9fdc47cb27b3e5
ascerba <a.scerba02@gmail.com>
2022-07-12 10:12:26 -0500

    Updated directory structure and moved to environment variables

--- raspi/DataUploadScripts/DataUpload.sh
+++ raspi/DataUploadScripts/DataUpload.sh
@@ -5,31 +5,21 @@
 # Copies all files with a creation date older than the current date to an archive directory
 # and then uploads them to Google Drive.
 #
-# @param $1 full path to directory containing sensor files. No trailing "/".
-# Defaults to '/home/pi/Local/sensorsdata'
-# @param $2 full path to dirictory containing archived sensor files. No trailing "/".
-# Defaults to '/home/pi/Local/SensorDataArchive'
-# @param $3 name of rclone location to use. Defaults to 'GLAS'
-
-inputDir=$1
-[ -z "inputDir" ] && inputDir="/home/pi/Local/sensorsdata"
-archiveDir=$2
-[ -z "archiveDir" ] && archiveDir="/home/pi/Local/SensorDataArchive"
-drive=$3
-[ -z "drive" ] && drive="GLAS"
+# Relies on the following environment variables stored in /etc/environment:
+# SENSOR_INPUT_PATH --> Location of directory that contains the sensor data files
+# SENSOR_ARCHIVE_PATH --> Location of archive directory
+# SENSOR_DRIVE --> Name of Rclone remote where the sensor data is stored
 
 # For every enty found, the date, month number, file name, and sensor number are stored.
 # The month name in found in a lookup table based off of the month number. The previously
 # stored variables are then used to create directories for storage and move the files to
 # their appropriate location.
-for output in $(find "$inputDir" -type f -printf '%T+;%p\n' | sed "/$(date +%Y-%m-%d)/d")
+for file in $(find "$SENSOR_INPUT_PATH" -type f -printf '%P\n' | sed "/$(date +%Y-%m-%d)/d")
 do
-    date=$(echo $output | awk -F ";" '{print substr($1,0,8)}')
-    monthNum=$(echo $date | awk '{print substr($1,6,2)}')
-    month=$(grep $monthNum ./monthLookup | awk '{print $2}')
-    fileName=$(echo $output | awk -F ";" '{print $2}')
-    sensor=$(echo $fileName | awk -F "/" '{print $(NF-1)}')
-    mkdir -p $archiveDir/$date$month/$sensor
-    cp "$fileName" "$archiveDir/$date$month/$sensor/"
-    rclone move "$fileName" $drive:$date$month/$sensor/
+    year=$(echo "$file" | awk '{print substr($0,1,4)}')
+    month=$(echo "$file" | awk '{print substr($0,6,2)}')
+    sensor="LENSS_TSL_$(echo $HOSTNAME | cut -c7-10)"
+    mkdir -p "$SENSOR_ARCHIVE_PATH/$sensor/$year/$month"
+    cp "$SENSOR_INPUT_PATH/$file" "$SENSOR_ARCHIVE_PATH/$sensor/$year/$month/"
+    rclone move "$SENSOR_INPUT_PATH/$file" "$SENSOR_DRIVE:$sensor/$year/$month/"
 done
