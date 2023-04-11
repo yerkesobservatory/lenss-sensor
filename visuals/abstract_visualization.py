115c9cd9281bb0922f9f38487b3bad0855609577
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-11 14:50:40 -0500

    Update abstract_visualization.py

---
+++ visuals/abstract_visualization.py
@@ -0,0 +1,5 @@
+# Define and abstract visualization class that loads data, parses it correctly,
+# and renders the HTML from it.
+# All methods should be @abstractmethods with docstrings underneath them that
+# tell you what the function should accomplish.
+# Look here for an example: https://github.com/chanteriam/reproductive-rights-data-project/blob/main/reproductive_rights_data_project/visualization/abstract_visualization.py
