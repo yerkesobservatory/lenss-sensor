5ce4c177e75cfe555ad95ac082892e8a2c9eb0a0
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-17 11:20:18 -0500

    Update abstract_visualization.py

--- visuals/abstract_visualization.py
+++ visuals/abstract_visualization.py
@@ -1,9 +1,3 @@
-# Define and abstract visualization class that loads data, parses it correctly,
-# and renders the HTML from it.
-# All methods should be @abstractmethods with docstrings underneath them that
-# tell you what the function should accomplish.
-# Look here for an example: https://github.com/chanteriam/reproductive-rights-data-project/blob/main/reproductive_rights_data_project/visualization/abstract_visualization.py
-
 from abc import ABC, abstractmethod
 
 
@@ -15,7 +9,7 @@ class Visualization(ABC):
     @abstractmethod
     def _import_files(self):
         """
-        This method pulls the relevant sensor data file for the given night
+        This method pulls the relevant sensor data file(s) for the given night
         and the following morning from Google Drive.
         """
         pass
@@ -23,24 +17,24 @@ class Visualization(ABC):
     @abstractmethod
     def _parse_data(self):
         """
-        This method cleans the data files and organizes it
-        with the relevant column names as a dataframe.
+        This method cleans the data files and organizes it with the relevant
+        column names as a dataframe.
         """
         pass
 
     @abstractmethod
     def _construct_data(self):
         """
-        This method constructs all the information needed for the
-        class to render the visualization.
+        This method constructs all the information needed for the class to
+        render the visualization.
         """
         pass
 
     @abstractmethod
     def _call_apis(self):
         """
-        This method pulls the relevant weather and moon phase data for each night
-        from external APIs.
+        This method pulls the relevant weather, moon phase, etc. data for each
+        night from external APIs.
         """
         pass
 
