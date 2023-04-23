8b26ade2bb4367ce4c46fc940c15e255d6af082f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-23 15:28:28 -0500

    Formatted the file

--- visuals/visual_one.py
+++ visuals/visual_one.py
@@ -1,19 +1,29 @@
-from abstract_visualization import Visualization
-import pandas as pd
-import os
-import requests
 import datetime
+import os
 from datetime import date, timedelta
+
 import matplotlib.pyplot as plt
+import pandas as pd
 import plotly.graph_objs as go
 import plotly.offline as pyo
+import requests
+
+from abstract_visualization import Visualization
+
 
 class Visual1(Visualization):
     """
     This abstract class is the one that all visualizations will be built on.
     """
 
-    def __init__(self, evening_file, morning_file, sensor_latitude, sensor_longitude, sensor_number):
+    def __init__(
+        self,
+        evening_file,
+        morning_file,
+        sensor_latitude,
+        sensor_longitude,
+        sensor_number,
+    ):
         self.evening_file = evening_file
         self.morning_file = morning_file
 
@@ -22,44 +32,60 @@ class Visual1(Visualization):
 
         self.sensor_number = sensor_number
 
-        self.morning_data, self.evening_data = self._import_files(), \
-                                            self._import_files()
-        
+        self.morning_data, self.evening_data = (
+            self._import_files(),
+            self._import_files(),
+        )
 
-    
     def _import_files(self):
         """
-        This method pulls the relevant sensor data file for the given night 
-        and the following morning from Google Drive. 
+        This method pulls the relevant sensor data file for the given night
+        and the following morning from Google Drive.
         """
         folder = "../streamlit/files/"
         file_path_morn = folder + self.morning_file
         file_path_eve = folder + self.evening_file
-        
-        col_names = ['Time (UTC)','Time (CST)','Temperature','Frequency','Voltage','Sensor']
-        df_morn = pd.read_csv(file_path_morn, low_memory=False, sep=";", names=col_names)
-        df_eve = pd.read_csv(file_path_eve, low_memory=False, sep=";", names=col_names)
+
+        col_names = [
+            "Time (UTC)",
+            "Time (CST)",
+            "Temperature",
+            "Frequency",
+            "Voltage",
+            "Sensor",
+        ]
+        df_morn = pd.read_csv(
+            file_path_morn, low_memory=False, sep=";", names=col_names
+        )
+        df_eve = pd.read_csv(
+            file_path_eve, low_memory=False, sep=";", names=col_names
+        )
 
         return df_morn, df_eve
-    
+
     def _parse_data(self, df):
         """
-        This method cleans the data files and organizes it 
+        This method cleans the data files and organizes it
         with the relevant column names as a dataframe.
         """
 
         # Assuming the timestamp column is stored in a variable called "timestamp_str"
-        timestamp_format = '%Y-%m-%dT%H:%M:%S.%f'  # Define the format of the timestamp
+        timestamp_format = (
+            "%Y-%m-%dT%H:%M:%S.%f"  # Define the format of the timestamp
+        )
 
-        df['Time (CST)'] = pd.to_datetime(df['Time (CST)'], format='%Y-%m-%dT%H:%M:%S.%f')
-        df["Time (UTC)"] = pd.to_datetime(df['Time (UTC)'], format='%Y-%m-%dT%H:%M:%S.%f')
+        df["Time (CST)"] = pd.to_datetime(
+            df["Time (CST)"], format="%Y-%m-%dT%H:%M:%S.%f"
+        )
+        df["Time (UTC)"] = pd.to_datetime(
+            df["Time (UTC)"], format="%Y-%m-%dT%H:%M:%S.%f"
+        )
 
         df["Day"] = df["Time (UTC)"].dt.day
         df["Month"] = df["Time (UTC)"].dt.month
         df["Year"] = df["Time (UTC)"].dt.year
 
         df["Date"] = df["Time (UTC)"].apply(lambda x: x.date())
-
 
     def _construct_data(self):
         """
@@ -70,17 +96,19 @@ class Visual1(Visualization):
 
     def _call_apis(self):
         """
-        This method pulls the relevant weather and moon phase data for each night 
-        from external APIs. 
+        This method pulls the relevant weather and moon phase data for each night
+        from external APIs.
         """
         api_url = f"https://api.open-meteo.com/v1/forecast?latitude={self.sensor_latitude}&longitude={self.sensor_longitude}&hourly=temperature_2m"
         response = requests.get(api_url)
         response.json()
 
-    def create_visual(self, df_morn, df_eve, t_start: datetime, t_end: datetime):
+    def create_visual(
+        self, df_morn, df_eve, t_start: datetime, t_end: datetime
+    ):
         """
         This method takes the data from the _construct_data function and returns
-        a visual object as an HTML file. 
+        a visual object as an HTML file.
         """
 
         df_morn_inbounds = df_morn[df_morn["Time (UTC)"] < t_end]
@@ -92,14 +120,23 @@ class Visual1(Visualization):
         y = df["Frequency"]
 
         # creating the line graph using plotly
-        trace = go.Scatter(x=x, y=y, mode='lines')
+        trace = go.Scatter(x=x, y=y, mode="lines")
         data = [trace]
-        layout = go.Layout(title=f'{df["Date"][0]} Sensor {self.sensor_number} Dark Sky Observation', xaxis=dict(title='Time (UTC)'), yaxis=dict(title='Light Frequency'))
+        layout = go.Layout(
+            title=f'{df["Date"][0]} Sensor {self.sensor_number} Dark Sky Observation',
+            xaxis=dict(title="Time (UTC)"),
+            yaxis=dict(title="Light Frequency"),
+        )
         fig = go.Figure(data=data, layout=layout)
-        pyo.plot(fig, filename='line_graph.html')
-
+        pyo.plot(fig, filename="line_graph.html")
 
 
 if __name__ == "__main__":
-    vis1 = Visual1("2022-08-27_LENSSTSL0008.txt", "2022-08-28_LENSSTSL0008.txt", 42.57, -88.542, 8)
+    vis1 = Visual1(
+        "2022-08-27_LENSSTSL0008.txt",
+        "2022-08-28_LENSSTSL0008.txt",
+        42.57,
+        -88.542,
+        8,
+    )
     vis1.create_visual()
