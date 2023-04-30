ea105891a61c69e24aa5a6ac416ecd7e1f36efb2
Fatimah Jallow <fjallow@uchicago.edu>
2023-04-30 15:46:08 -0500

    pushing to save because of vscode malfunction; errors likely

--- data_analysis/correction_framework.py
+++ data_analysis/correction_framework.py
@@ -1,20 +1,20 @@
 # -> setup a virtual environment and download packages via `pip install [package]`
 
+import os
+
 # include library python-weather at https://github.com/null8626/python-weather
 # might switch to a weather api?
-
-import astroplan
-import astropy
-import astropy.units as u
+import python_weather
 
 # include astropy and astroplan (astropy interface)
-import python_weather
+import astroplan
 from astroplan import Observer
+import astropy
+import astropy.units as u
 from astropy.coordinates import EarthLocation
 from astropy.time import Time
 
 Field = Enum("Field", ["TEMP", "FREQ", "VOLT"])
-
 
 class minute_record:
     def __init__(
@@ -35,96 +35,112 @@ class minute_record:
 
 
 class night_record:
-    def __init__(
+    # create an uninitialized night record
+    def __init__(self):
+        self.nightof = 0
+        self.morningof = 0
+        self.sunset = 0
+        self.sunrise = 0
+        self.astronomical_twilight = 0
+        self.moon_illumination = 0
+        self.weather = 0  
+        self.min_records = []
+        self.valid_for_use = False
+        self.exclusion_reason = "UNITINIALIZED"
+
+    # initialize a night record from a raw data file
+    def initialize(
         self,
         night_date: Time,
         night_file: str,
         morning_date: Time,
         morning_file: str,
-    ):
-        # let night and morning data include time as noon of their respective days
-        # time = Time('2015-06-16 12:00:00') 2023-01-23T06:00:00.992
+        sensor_long: float
+        sensor_lat: float
+        ):
+            # let night and morning date include time as noon of their respective days
+            # Format Reminders: time = Time('2015-06-16 12:00:00') for assignment,
+            #                          2023-01-23T06:00:00.992 on file
 
-        self.nightof = night_date
-        self.morningof = morning_date
+            self.nightof = night_date
+            self.morningof = morning_date
 
-        sensor = Observer(
-            longitude=-155.4761 * u.deg,
-            latitude=19.825 * u.deg,
-            elevation=0 * u.m,
-            name="Sensor",
-            timezone="US/Central",
-        )  # TODO Update Location
-        self.sunset = sensor.subaru.sun_set_time(night_date, which="next")
-        self.sunrise = sensor.subaru.sun_rise_time(night_date, which="next")
-        self.astronomical_twilight = sensor.twilight_evening_astronomical(
-            night_date, which="next"
-        )
-
-        # how much of the lunar surface is illuminated that night, pulled when the sky is fully dark (astro. twilight)
-        # comparable to moon phase (0 = new moon, ~1 = full) without the pi of obs.moon_phase
-        self.moon_illumination = sensor.moon_illumination(
-            self.astronomical_twilight
-        )
-        self.weather = 0  # TODO Update Placeholder -- how to succinctly record nightly weather?
-
-        # example line from 2023-01-23_LENSSTSL0008.txt:
-        # 2023-01-23T06:00:00.992;2023-01-23T00:00:00.992;0.38;4.39;0.0;LENSS_TSL_0008
-        # UTC Time; Local Time; Temperature (C); Frequency; Voltage; ID
-
-        self.min_records = []
-
-        n_file = open(night_file)
-        transtr = n_file.readline()
-        while len(transtr) != 0:
-            data = transtr.split(";")
-            data[0] = data[0].replace("T", " ")
-            data[0] = data[0][0:19]
-            data[1] = data[1].replace("T", " ")
-            data[1] = data[1][0:19]
-            m_rec = minute_record(
-                Time(data[0]),
-                Time(data[1]),
-                data[2].float(),
-                data[3].float(),
-                data[4].float,
-                data[5],
+            sensor = Observer(
+                longitude=sensor_long * u.deg,
+                latitude=sensor_lat * u.deg,
+                elevation=0 * u.m,
+                name="Sensor",
+                timezone="US/Central",
+            ) 
+            self.sunset = sensor.sun_set_time(night_date, which="next")
+            self.sunrise = sensor.sun_rise_time(night_date, which="next")
+            self.astronomical_twilight = sensor.twilight_evening_astronomical(
+                night_date, which="next"
             )
-            if (
-                m_rec.local_time < self.sunset
-            ):  # unsure if this will work or if a special time comp function is needed
-                continue
-            self.min_records.append(m_rec)
-            n_file.readline()
-        n_file.close()
 
-        m_file = open(morning_file)
-        transtr = m_file.readline()
-        while len(transtr) != 0:
-            data = transtr.split(";")
-            data[0] = data[0].replace("T", " ")
-            data[0] = data[0][0:19]
-            data[1] = data[1].replace("T", " ")
-            data[1] = data[1][0:19]
-            m_rec = minute_record(
-                Time(data[0]),
-                Time(data[1]),
-                data[2].float(),
-                data[3].float(),
-                data[4].float,
-                data[5],
+            # how much of the lunar surface is illuminated that night, pulled when the sky is fully dark (astro. twilight)
+            # comparable to moon phase (0 = new moon, ~1 = full) without the pi of obs.moon_phase
+            self.moon_illumination = sensor.moon_illumination(
+                self.astronomical_twilight
             )
-            if (
-                m_rec.local_time > self.sunrise
-            ):  # unsure if this will work or if a special time comp function is needed
-                break
-            self.min_records.append(m_rec)
-            n_file.readline()
-        m_file.close()
+            self.weather = 0  # TODO Update Placeholder -- how to succinctly record nightly weather?
 
-        self.valid_for_use = True
-        self.exclusion_reason = "None"
+            # example line from 2023-01-23_LENSSTSL0008.txt:
+            # 2023-01-23T06:00:00.992;2023-01-23T00:00:00.992;0.38;4.39;0.0;LENSS_TSL_0008
+            # UTC Time; Local Time; Temperature (C); Frequency; Voltage; ID
 
+            self.min_records = []
+
+            n_file = open(night_file)
+            transtr = n_file.readline()
+            while len(transtr) != 0:
+                data = transtr.split(";")
+                data[0] = data[0].replace("T", " ")
+                data[1] = data[1].replace("T", " ")
+                m_rec = minute_record(
+                    Time(data[0]),
+                    Time(data[1]),
+                    data[2].float(),
+                    data[3].float(),
+                    data[4].float,
+                    data[5],
+                )
+                if (
+                    m_rec.local_time < self.sunset
+                ):  # unsure if this will work or if a special time comp function is needed
+                    n_file.readline()
+                    continue
+                self.min_records.append(m_rec)
+                transtr = n_file.readline()
+            n_file.close()
+
+            m_file = open(morning_file)
+            transtr = m_file.readline()
+            while len(transtr) != 0:
+                data = transtr.split(";")
+                data[0] = data[0].replace("T", " ")
+                data[1] = data[1].replace("T", " ")
+                m_rec = minute_record(
+                    Time(data[0]),
+                    Time(data[1]),
+                    data[2].float(),
+                    data[3].float(),
+                    data[4].float,
+                    data[5],
+                )
+                if (
+                    m_rec.local_time > self.sunrise
+                ):  # unsure if this will work or if a special time comp function is needed
+                    m_file.readline()
+                    break
+                self.min_records.append(m_rec)
+                transtr = m_file.readline()
+            m_file.close()
+
+            self.valid_for_use = True
+            self.exclusion_reason = "None"
+
+    # mark data as invalid based on various night conditions
     def grade_data(self):
         # taking the average night to be ten hours (600 minutes), if more than three hours (180 minutes) are missing,
         # invalidate the data based on insufficient records
@@ -133,12 +149,12 @@ class night_record:
             self.exclusion_reason = "Insufficient Records for Night"
 
         # if the moon would have been obscured by bad weather for more then three hours, invalidate based on weather conditions
-        counter = 0
         # use something other than python-weather?
 
+
+    # given a function on a float, updates all temperature, frequency, or voltage values
     def apply_correction(self, field_to_update, funct):
         match (field_to_update):
-            # TODO Adjust for Switch Case/Enum Weirdness in Python
             case TEMP:
                 for rec in self.min_records:
                     rec.temperature = funct(rec.temperature)
@@ -149,8 +165,87 @@ class night_record:
                 for rec in self.min_records:
                     rec.voltage = funct(rec.voltage)
             case _:
-                print("error")
-                # error: field not recognized/field should not be updated
+                print("error: field not recognized/field should not be updated")
 
-    # export
-    # check if valid for use and if not, exclusion reason
+    # export night record to a new file
+    def export(self, filename: str):
+        new_file = open(filename, "x")
+        if (self.valid_for_use == FALSE):
+            new_file.write("INVALID\n")
+            new_file.write(self.exclusion_reason)
+        else:
+            # night date -- 2023-01-23 06:00:00.992
+            new_file.write(self.night_date.strptime('%Y-%m-%d %H:%M:%S.%f'))
+            new_file.write("\n")
+            # morning date
+            new_file.write(self.morning_date.strptime('%Y-%m-%d %H:%M:%S.%f'))
+            new_file.write("\n")
+            # sunrise
+            new_file.write(self.sunrise.strptime('%Y-%m-%d %H:%M:%S.%f'))
+            new_file.write("\n")
+            # sunset
+            new_file.write(self.sunset.strptime('%Y-%m-%d %H:%M:%S.%f'))
+            new_file.write("\n")
+            # twilight
+            new_file.write(self.astronomical_twilight.strptime('%Y-%m-%d %H:%M:%S.%f'))
+            new_file.write("\n")
+            # illumination
+            new_file.write(str(self.moon_illumination))
+            new_file.write("\n")
+            # weather
+            # unimplemented
+
+            for rec in self.min_records:
+                new_file.write(rec.utc_timesunset.strptime('%Y-%m-%d %H:%M:%S.%f'))
+                new_file.write(";")
+                new_file.write(rec.local_time.strptime('%Y-%m-%d %H:%M:%S.%f'))
+                new_file.write(";")
+                new_file.write(str(rec.temperature))
+                new_file.write(";")
+                new_file.write(str(rec.frequency))
+                new_file.write(";")
+                new_file.write(str(rec.voltage))
+                new_file.write(";")
+                new_file.write(str(rec.sensor_id))
+                new_file.write("\n")
+        new_file.close()
+
+    # import previously exported night record
+    def import(self, filename: str):
+        #takes an already created object and updates it to imported values
+        file = open(filename)
+        p_str = file.readline()
+        if p_str == "INVALID":
+            # check if this is okay for str comp?
+            self.valid_for_use = FALSE
+            self.exclusion_reason = file.readline()
+        else:
+          # night date -- 2023-01-23 06:00:00.992
+            self.night_date = Time(p_str)
+            # morning date
+            self.morning_date = Time(file.readline())
+            # sunrise
+            self.sunrise = Time(file.readline())
+            # sunset
+            self.sunset = Time(file.readline())
+            # twilight
+            self.astronomical_twilight = Time(file.readline())
+            # illumination
+            self.moon_illumination = file.readline().float()
+            # weather
+            # unimplemented  
+
+            p_str = file.readline()
+            while len(p_str) != 0:
+                data = p_str.split(";")
+                m_rec = minute_record(
+                    Time(data[0]),
+                    Time(data[1]),
+                    data[2].float(),
+                    data[3].float(),
+                    data[4].float,
+                    data[5],
+                )
+                self.min_records.append(m_rec)
+                p_str = file.readline()
+        file.close()
