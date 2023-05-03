fe3b9c6baf4d8597192e86403e1a7384f2af40a9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-02 22:40:36 -0500

    Update correction_framework.py

--- data_analysis/correction_framework.py
+++ data_analysis/correction_framework.py
@@ -3,12 +3,12 @@ import os
 
 import astropy.units as u
 from astroplan import Observer
-from astropy.coordinates import EarthLocation
 from astropy.time import Time
 
-PROJECT_DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+PROJECT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 
-class minute_record:
+
+class MinuteRecord:
     def __init__(
         self,
         utc: Time,
@@ -26,11 +26,11 @@ class minute_record:
         self.sensor_id = id
 
 
-class night_record:
+class NightRecord:
     # create an uninitialized night record
     def __init__(self):
-        self.nightof = 0
-        self.morningof = 0
+        self.night_of = 0
+        self.morning_of = 0
         self.sunset = 0
         self.sunrise = 0
         self.astronomical_twilight = 0
@@ -50,11 +50,12 @@ class night_record:
         sensor_lat: float,
         sensor_long: float,
     ):
-        # let night and morning date include time as noon of their respective days
+        # let night and morning date include time as noon of their respective
+        # days
         # Format Reminders: time = Time('2015-06-16 12:00:00') for assignment,
         #                          2023-01-23T06:00:00.992 on file
-        self.nightof = night_date
-        self.morningof = morning_date
+        self.night_of = night_date
+        self.morning_of = morning_date
 
         sensor = Observer(
             longitude=sensor_long * u.deg,
@@ -64,14 +65,15 @@ class night_record:
             timezone="US/Central",
         )
         # reports time in UTC
-        self.sunset = sensor.sun_set_time(night_date, which='nearest')
+        self.sunset = sensor.sun_set_time(night_date, which="nearest")
         self.sunrise = sensor.sun_rise_time(morning_date, which="nearest")
         self.astronomical_twilight = sensor.twilight_evening_astronomical(
             night_date, which="nearest"
         )
 
-        # how much of the lunar surface is illuminated that night, pulled when the sky is fully dark (astro. twilight)
-        # comparable to moon phase (0 = new moon, ~1 = full) without the pi of obs.moon_phase
+        # how much of the lunar surface is illuminated that night, pulled when
+        # the sky is fully dark (astro. twilight) comparable to moon phase
+        # (0 = new moon, ~1 = full) without the pi of obs.moon_phase
         self.moon_illumination = sensor.moon_illumination(
             self.astronomical_twilight
         )
@@ -83,12 +85,12 @@ class night_record:
         # 2023-01-23T06:00:00.992;2023-01-23T00:00:00.992;0.38;4.39;0.0;LENSS_TSL_0008
         # UTC Time; Local Time; Temperature (C); Frequency; Voltage; ID
         with open(night_file) as n_file:
-            transtr = n_file.readline()
-            while len(transtr) != 0:
-                data = transtr.split(";")
+            trans_str = n_file.readline()
+            while len(trans_str) != 0:
+                data = trans_str.split(";")
                 data[0] = data[0].replace("T", " ")
                 data[1] = data[1].replace("T", " ")
-                m_rec = minute_record(
+                m_rec = MinuteRecord(
                     Time(data[0]),
                     Time(data[1]),
                     float(data[2]),
@@ -96,21 +98,19 @@ class night_record:
                     float(data[4]),
                     data[5],
                 )
-                if (
-                    m_rec.utc_time < self.sunset
-                ):
-                    transtr = n_file.readline()
+                if m_rec.utc_time < self.sunset:
+                    trans_str = n_file.readline()
                     continue
                 self.min_records.append(m_rec)
-                transtr = n_file.readline()
+                trans_str = n_file.readline()
 
         with open(morning_file) as m_file:
-            transtr = m_file.readline()
-            while len(transtr) != 0:
-                data = transtr.split(";")
+            trans_str = m_file.readline()
+            while len(trans_str) != 0:
+                data = trans_str.split(";")
                 data[0] = data[0].replace("T", " ")
                 data[1] = data[1].replace("T", " ")
-                m_rec = minute_record(
+                m_rec = MinuteRecord(
                     Time(data[0]),
                     Time(data[1]),
                     float(data[2]),
@@ -118,28 +118,29 @@ class night_record:
                     float(data[4]),
                     data[5],
                 )
-                if (
-                    m_rec.utc_time > self.sunrise
-                ): 
+                if m_rec.utc_time > self.sunrise:
                     break
                 self.min_records.append(m_rec)
-                transtr = m_file.readline()
+                trans_str = m_file.readline()
 
         self.valid_for_use = True
         self.exclusion_reason = "None"
 
     # grade quality of as based on various night conditions
     def grade_data(self):
-        # taking the average night to be ten hours (600 minutes), if more than three hours (180 minutes) are missing,
-        # invalidate the data based on insufficient records
+        # taking the average night to be ten hours (600 minutes), if more than
+        # three hours (180 minutes) are missing, invalidate the data based on
+        # insufficient records
         if len(self.min_records) < 420:
             self.valid_for_use = False
             self.exclusion_reason = "Insufficient Records for Night"
 
         # future TODO: invalidate based on weather conditions
-        # potential future TODO: grade data on a scale (good, poor, unusable, etc) instead of just valid or invalid
+        # potential future TODO: grade data on a scale (good, poor, unusable,
+        #  etc) instead of just valid or invalid
 
-    # given a function on a float, updates all temperature, frequency, or voltage values
+    # given a function on a float, updates all temperature, frequency, or
+    # voltage values
     def apply_correction(self, field_to_update: str, funct):
         if field_to_update == "TEMP":
             for rec in self.min_records:
@@ -165,7 +166,7 @@ class night_record:
                     self.sunrise.strftime(PROJECT_DATE_FORMAT),
                     self.sunset.strftime(PROJECT_DATE_FORMAT),
                     self.astronomical_twilight.strftime(PROJECT_DATE_FORMAT),
-                    str(self.moon_illumination)
+                    str(self.moon_illumination),
                 ]
                 new_file.write("\n".join(vals) + "\n")
 
@@ -176,7 +177,7 @@ class night_record:
                         str(rec.temperature),
                         str(rec.frequency),
                         str(rec.voltage),
-                        rec.sensor_id
+                        rec.sensor_id,
                     ]
                     new_file.write(";".join(vals) + "\n")
 
@@ -205,7 +206,7 @@ class night_record:
                 p_str = file.readline()
                 while len(p_str) != 0:
                     data = p_str.split(";")
-                    m_rec = minute_record(
+                    m_rec = MinuteRecord(
                         Time(data[0]),
                         Time(data[1]),
                         float(data[2]),
@@ -216,37 +217,74 @@ class night_record:
                     self.min_records.append(m_rec)
                     p_str = file.readline()
 
+
 def cor_frmwrk_tests():
     # run with "python correction_framework.py cor_frmwrk_tests" on command line
-    nrec = night_record()
-    
+    nrec = NightRecord()
+
     print("starting simple test:")
-    '''
+    """
     simple test will use simple_test_night.txt and simple_test_morning.txt on 
-    the night of 2023/01/24 and morning of 2023/01/25, where moon illumination is 9.9%, 
-    sunset is 4:55 PM CT/9:55 PM UTC, sunrise is 7:09 AM CT/12:09 PM UTC, and astronomical 
-    twilight is between 05:59 PM CT/10:59 PM UTC and 6:32 PM CT/11:32 PM UTC in Hyde Park
-    '''
+    the night of 2023/01/24 and morning of 2023/01/25, where moon illumination 
+    is 9.9%, sunset is 4:55 PM CT/9:55 PM UTC, sunrise is 7:09 AM CT/12:09 PM 
+    UTC, and astronomical twilight is between 05:59 PM CT/10:59 PM UTC and 6:32 
+    PM CT/11:32 PM UTC in Hyde Park
+    """
     print("working directory: " + os.getcwd() + "\n")
-    nrec.initialize(Time("2023-01-24 12:00:00.000"), "./test_files/simple_test_night.txt", 
-                    Time("2023-01-25 12:00:00.000"), "./test_files/simple_test_morning.txt",
-                    41.7948, -87.5917)
-    print("night date should be 23/1/24: " + nrec.nightof.strftime(PROJECT_DATE_FORMAT))
-    print("morning date should be 23/1/25: " + nrec.morningof.strftime(PROJECT_DATE_FORMAT) + "\n")
+    nrec.initialize(
+        Time("2023-01-24 12:00:00.000"),
+        "./test_files/simple_test_night.txt",
+        Time("2023-01-25 12:00:00.000"),
+        "./test_files/simple_test_morning.txt",
+        41.7948,
+        -87.5917,
+    )
+    print(
+        "night date should be 23/1/24: "
+        + nrec.night_of.strftime(PROJECT_DATE_FORMAT)
+    )
+    print(
+        "morning date should be 23/1/25: "
+        + nrec.morning_of.strftime(PROJECT_DATE_FORMAT)
+        + "\n"
+    )
 
-    print("sunrise, sunset, and twilight are off by one hour, likely due to daylight savings; otherwise within reasonable range (~5m)")
-    print("sunset should be ~21:55: " + nrec.sunset.strftime(PROJECT_DATE_FORMAT))
-    print("sunrise should be ~12:09: " + nrec.sunrise.strftime(PROJECT_DATE_FORMAT))
-    print("twilight should be between 10:59 and 11:32: " + nrec.astronomical_twilight.strftime(PROJECT_DATE_FORMAT) + "\n")
+    print(
+        "sunrise, sunset, and twilight are off by one hour, likely due to "
+        "daylight savings; otherwise within reasonable range (~5m)"
+    )
+    print(
+        "sunset should be ~21:55: " + nrec.sunset.strftime(PROJECT_DATE_FORMAT)
+    )
+    print(
+        "sunrise should be ~12:09: "
+        + nrec.sunrise.strftime(PROJECT_DATE_FORMAT)
+    )
+    print(
+        "twilight should be between 10:59 and 11:32: "
+        + nrec.astronomical_twilight.strftime(PROJECT_DATE_FORMAT)
+        + "\n"
+    )
 
-    print("illumination is way off but that could be indicative of real world conditions in Chicago rather than an error")
+    print(
+        "illumination is way off but that could be indicative of real world "
+        "conditions in Chicago rather than an error"
+    )
     print("illumination should be ~9.9: " + str(nrec.moon_illumination) + "\n")
 
     print("should be 2 records: " + str(len(nrec.min_records)))
-    print("first record should be 23:00: " + nrec.min_records[0].utc_time.strftime(PROJECT_DATE_FORMAT))
-    print("second record should be 10:00: " + nrec.min_records[1].utc_time.strftime(PROJECT_DATE_FORMAT) + "\n")
+    print(
+        "first record should be 23:00: "
+        + nrec.min_records[0].utc_time.strftime(PROJECT_DATE_FORMAT)
+    )
+    print(
+        "second record should be 10:00: "
+        + nrec.min_records[1].utc_time.strftime(PROJECT_DATE_FORMAT)
+        + "\n"
+    )
 
     print("exclusion should be none: " + nrec.exclusion_reason)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     globals()[sys.argv[1]]()
