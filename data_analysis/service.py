f8406020637621d70a586404faaa22395780dec5
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-07 17:07:02 -0500

    Create service.py

---
+++ data_analysis/service.py
@@ -0,0 +1,12 @@
+"""
+This file serves as the central runner for any data analysis processes.
+"""
+
+
+def main():
+    """
+    This function is the central function for any data analysis or cleaning
+    processes.
+    """
+    # TODO: Move function calls to here so they go through a central point.
+    print("Doing things.")
