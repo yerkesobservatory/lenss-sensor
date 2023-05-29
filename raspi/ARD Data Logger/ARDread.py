bde4e04283b7079a6757c902000edf3f1ce645ff
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-29 16:15:33 -0500

    Formatting

--- raspi/ARD Data Logger/ARDread.py
+++ raspi/ARD Data Logger/ARDread.py
@@ -20,7 +20,7 @@ import numpy as np
 import pytz
 
 # fmt represents the number of decimal places each data point shows
-fmt = ['%.2f','%.2f','%.1f']
+fmt = ["%.2f", "%.2f", "%.1f"]
 
 # total number of deleted data points
 dltd = 0
@@ -29,21 +29,23 @@ ldltd = 0
 # time when program was launched to give dltd a reference
 starttime = datetime.now().strftime("%H:%M:%S, %Y-%m-%d")
 
+
 ### Function Definitions
 def getardport(config):
     # Returns the port string for the arduino
     try:
-        return config['arddatalogger']['arduinoport']
+        return config["arddatalogger"]["arduinoport"]
     except:
         exit(1)
+
 
 ### Main program
 
 # Checks if there are enough arguments in the command to setup config in next section
 if len(sys.argv) < 2:
-    print('WARNING: Must give filepathname to valid config file')
-    print('Usage:\n  python SQM_LU_LOGGER.py ../config/serverconfig.ini')
-    print('  replace with your own config file as appropriate')
+    print("WARNING: Must give filepathname to valid config file")
+    print("Usage:\n  python SQM_LU_LOGGER.py ../config/serverconfig.ini")
+    print("  replace with your own config file as appropriate")
     exit(1)
 
 # Setup configuration
@@ -52,32 +54,37 @@ config = configparser.ConfigParser()
 config.read(Config_FilePathName)
 
 # Setup background logging (time of operation and deleted data points)
-now=datetime.now()
+now = datetime.now()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-hand = TimedRotatingFileHandler(now.strftime(config['logging']['ardlogfile']), when="midnight")
+hand = TimedRotatingFileHandler(
+    now.strftime(config["logging"]["ardlogfile"]), when="midnight"
+)
 hand.suffix = "%Y-%m-%d.log"
 logformat = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 hand.setFormatter(logging.Formatter(logformat))
 logger.addHandler(hand)
-print(config['arddatalogger']['arduinoport'])
+print(config["arddatalogger"]["arduinoport"])
 
 # Open serial connection
-if int(config['arddatalogger']['simarduino']):
+if int(config["arddatalogger"]["simarduino"]):
     # We are simulating the arduino set serial to null
     ser = None
 else:
-    ser = serial.Serial( port=getardport(config),\
-                         baudrate=9600,\
-                         parity=serial.PARITY_NONE,\
-                         stopbits=serial.STOPBITS_ONE,\
-                         bytesize=serial.EIGHTBITS,\
-                         timeout=5 )
+    ser = serial.Serial(
+        port=getardport(config),
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=5,
+    )
     ser.readline()
+
 
 def serialread(config):
     try:
-        """ Read the serial value from the arduino and writes
+        """Read the serial value from the arduino and writes
         it to the file.
         """
         time.sleep(1)
@@ -91,25 +98,25 @@ def serialread(config):
 
         while True:
             time.sleep(2)
-            timestamp=time.time()
+            timestamp = time.time()
 
             # When entering a new minute (marked by the seconds decreasing after a loop from 58 to 01 or 59 to 02 or so on)
             # the data gathered is sorted from least to greatest, the top and bottom fifths of it are removed to get rid of
             # outliers, and the rest is averaged to produce a data string for the minute.
-            if (last > time.gmtime().tm_sec):
+            if last > time.gmtime().tm_sec:
                 for l in range(len(sl)):
                     sl[l].sort()
-                    sl[l] = sl[l][len(sl[l])//5:-(len(sl[l])//5)]
-                    sl[l]=fmt[l] % np.mean(sl[l])
+                    sl[l] = sl[l][len(sl[l]) // 5 : -(len(sl[l]) // 5)]
+                    sl[l] = fmt[l] % np.mean(sl[l])
                 break
             # Read most recent value from Arduino
             if ser:
-                read_ser=""
-                while(ser.in_waiting):
-                    read_ser=ser.readline()
+                read_ser = ""
+                while ser.in_waiting:
+                    read_ser = ser.readline()
                 read_fmtd = read_ser.decode("utf-8")
             else:
-                read_fmtd = config['arddatalogger']['simardline']
+                read_fmtd = config["arddatalogger"]["simardline"]
 
             # Read data from string and tracks the number of deleted data points
             sdata = read_fmtd.split(";")
@@ -118,34 +125,47 @@ def serialread(config):
                 for i in range(len(sdata)):
                     sl[i].append(float(sdata[i]))
             except:
-                dltd+=1
+                dltd += 1
             print(sdata)
             print(time.gmtime().tm_sec)
             last = time.gmtime().tm_sec
 
         # Compile text for file
         read_timed = []
-        read_timed.append(datetime.utcfromtimestamp(timestamp).isoformat(sep='T', timespec='milliseconds'))
-        read_timed.append(datetime.fromtimestamp(timestamp, pytz.timezone('US/Central')).isoformat(sep='T', timespec='milliseconds')[:-6])
+        read_timed.append(
+            datetime.utcfromtimestamp(timestamp).isoformat(
+                sep="T", timespec="milliseconds"
+            )
+        )
+        read_timed.append(
+            datetime.fromtimestamp(
+                timestamp, pytz.timezone("US/Central")
+            ).isoformat(sep="T", timespec="milliseconds")[:-6]
+        )
         read_timed += sl
-        read_timed.append(config['arddatalogger']['ID'])
+        read_timed.append(config["arddatalogger"]["ID"])
         text_timed = ";".join(read_timed)
         print(text_timed)
         print(str(dltd) + " pieces of data deleted")
         # Make filename
-        fname = datetime.fromtimestamp(timestamp, pytz.timezone('US/Central')).strftime(config['arddatalogger']['outfilename'])
+        fname = datetime.fromtimestamp(
+            timestamp, pytz.timezone("US/Central")
+        ).strftime(config["arddatalogger"]["outfilename"])
         # Save to file
-        data = open(fname, 'at')
-        data.write(text_timed+"\r\n")
-        if (dltd-ldltd >= 100):
-            logger.info(str(dltd)+" lines deleted since "+starttime+"\r\n")
+        data = open(fname, "at")
+        data.write(text_timed + "\r\n")
+        if dltd - ldltd >= 100:
+            logger.info(
+                str(dltd) + " lines deleted since " + starttime + "\r\n"
+            )
             ldltd = dltd
         data.close()
-        logger.info('Connected to:' + str(getardport(config)))
-        logger.info('Read data line')
+        logger.info("Connected to:" + str(getardport(config)))
+        logger.info("Read data line")
     except:
         time.sleep(1)
 
-while(True):
+
+while True:
     serialread(config)
-    #time.sleep(1)
+    # time.sleep(1)
--- raspi/SQM LU Data Logger/SQM_LU_LOGGER.py
+++ raspi/SQM LU Data Logger/SQM_LU_LOGGER.py
@@ -25,9 +25,9 @@ from logging.handlers import TimedRotatingFileHandler
 
 # Get config file name from argument
 if len(sys.argv) < 2:
-    print('WARNING: Must give filepathname to valid config file')
-    print('Usage:\n  python SQM_LU_LOGGER.py ../config/serverconfig.ini')
-    print('  replace with your own config file as appropriate')
+    print("WARNING: Must give filepathname to valid config file")
+    print("Usage:\n  python SQM_LU_LOGGER.py ../config/serverconfig.ini")
+    print("  replace with your own config file as appropriate")
     exit(1)
 
 Config_FilePathName = sys.argv[1]
@@ -38,58 +38,64 @@ now = datetime.now()
 last = time.gmtime().tm_sec
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-hand = TimedRotatingFileHandler(now.strftime(config['logging']['sqmlogfile']),when="midnight")
+hand = TimedRotatingFileHandler(
+    now.strftime(config["logging"]["sqmlogfile"]), when="midnight"
+)
 hand.suffix = "%Y-%m-%d.log"
 logformat = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 hand.setFormatter(logging.Formatter(logformat))
 logger.addHandler(hand)
-#log = open('./sqmlu_' + now.strftime('%Y-%m-%d') + '.txt', 'a')
+# log = open('./sqmlu_' + now.strftime('%Y-%m-%d') + '.txt', 'a')
+
 
 # Automatically selects port syntax based on OS
 # IMPORTANT!!! - Assumes no other devices plugged in with 'USB Serial'
 # string and assumes similar model number.
 def port(config):
     try:
-        return config['sqmludatalogger']['sqmport']
+        return config["sqmludatalogger"]["sqmport"]
     except:
         pass
-    if str(platform.system()) == 'Windows':
+    if str(platform.system()) == "Windows":
         for p in ports:
-            if 'USB Serial' in p.description:
+            if "USB Serial" in p.description:
                 q = str(p)
-                return(q[:4])
+                return q[:4]
     else:
-        return  '/dev/tty.usbserial-A5055IX5A' # /dev/ttyS(Port Number) for WSL
+        return "/dev/tty.usbserial-A5055IX5A"  # /dev/ttyS(Port Number) for WSL
+
 
 # Main read function. Sends a string to the defined port and
 # prints the resulting output to a file "log.txt" and the terminal.
 def serialread(config):
     global last
-    if(last > time.gmtime().tm_sec):
+    if last > time.gmtime().tm_sec:
         now = datetime.now()
-        filename = now.strftime(config['sqmludatalogger']['outfilename'])
+        filename = now.strftime(config["sqmludatalogger"]["outfilename"])
         timestring = now.strftime("%H:%M:%S,")
         ser = serial.Serial(
-            port=port(config),\
-            baudrate=115200,\
-            parity=serial.PARITY_NONE,\
-            stopbits=serial.STOPBITS_ONE,\
-            bytesize=serial.EIGHTBITS,\
-                timeout=1)
+            port=port(config),
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1,
+        )
         print("connected to: " + ser.portstr)
-        logger.info('Connected to:' + str(ser.portstr))
+        logger.info("Connected to:" + str(ser.portstr))
         ser.write(str.encode("rx\n"))
         ser.flush()
         serline = ser.readline()
         serline_utf = serline.decode("utf-8")
-        dataline=timestring+serline_utf+"\n"
+        dataline = timestring + serline_utf + "\n"
         print(dataline)
         # log.write(str(ser.readline()) + "\n")
-        logfile = open(filename, 'at')
-        logfile.write(timestring+serline_utf+"\n")
-        logger.info('Read data line')
+        logfile = open(filename, "at")
+        logfile.write(timestring + serline_utf + "\n")
+        logger.info("Read data line")
         logfile.close()
     last = time.gmtime().tm_sec
+
 
 # Runs serialread() function every second.
 while True:
