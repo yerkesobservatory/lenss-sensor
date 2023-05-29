c390fedbdbe4b7d0b5253ecc76f0b41e381d9453
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-29 16:27:19 -0500

    Updated documentation and formattted the 🐍 files (#20)
    ## Describe your changes and motivations for this PR
    Updated the documentation in the `README.md` file and formatted/linted
    the python files in the repository.

--- zArchive/watchdog/watchdog.py
+++ zArchive/watchdog/watchdog.py
@@ -1,15 +1,16 @@
-import smtplib, ssl
-import os, sys
-import configparser
 from datetime import datetime
-import time
+import configparser
+import os
+import smtplib
+import ssl
+import sys
 
 Password_FilePathName = sys.argv[2]
 readPassword = configparser.ConfigParser()
 readPassword.read(Password_FilePathName)
 
 port = 465
-password = readPassword['email']['password']
+password = readPassword["email"]["password"]
 smtp_server = "smtp.gmail.com"
 sender = "lenssserver@gmail.com"
 receivers = ["joe.murphy1415@gmail.com", "adam@glaseducation.org"]
@@ -20,26 +21,30 @@ config.read(Config_FilePathName)
 
 context = ssl.create_default_context()
 
+
 def sendstatus():
     now = datetime.now()
-    filename = now.strftime(config['arddatalogger']['outfilename'])
+    filename = now.strftime(config["arddatalogger"]["outfilename"])
     try:
         lastmodified = os.stat(filename).st_mtime
     except:
         lastmodified = 0
-    TimeLM = datetime.fromtimestamp(lastmodified)
+    datetime.fromtimestamp(lastmodified)
     UnixNow = now.timestamp()
-    timeDelta = UnixNow-lastmodified
+    timeDelta = UnixNow - lastmodified
 
     message = """\
     Subject: Daily Status Report
 
-    It has been %d second(s) since %s has been updated.""" %(timeDelta, filename)
+    It has been %d second(s) since %s has been updated.""" % (
+        timeDelta,
+        filename,
+    )
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender, password)
         for address in receivers:
-           server.sendmail(sender, address, message)
+            server.sendmail(sender, address, message)
 
 
 sendstatus()
